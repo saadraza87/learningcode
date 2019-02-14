19618e11c037f4cd6d1d9655352e6c7f32fbf603
Saad Raza Abbasi <saadabbasi.iba@gmail.com>
2019-02-14 15:22:36 +0500

    Response get successful

--- learnpython/hmb_forex/bs4_forex.py
+++ learnpython/hmb_forex/bs4_forex.py
@@ -5,3 +5,9 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+
+#Testing simple_get
+
+test_html = get('https://www.brecorder.com/exchange-rates/', stream=True)
+print(test_html)
+test_html
